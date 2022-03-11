0897742514b5291d7cb6d4b7707fb72534ce931d
Marius_Gurgu <mihaiandrei1987@yahoo.com>
2022-03-11 15:38:42 +0200

    added timestamp to pose converter

--- pose_to_vis_odom.py
+++ pose_to_vis_odom.py
@@ -3,6 +3,9 @@ from rclpy.node import Node
 
 from geometry_msgs.msg import PoseStamped
 from px4_msgs.msg import VehicleVisualOdometry
+from px4_msgs.msg import VehicleMocapOdometry
+from px4_msgs.msg import Timesync
+
 
 
 class MinimalSubscriber(Node):
@@ -12,32 +15,52 @@ class MinimalSubscriber(Node):
         self.subscription = self.create_subscription(
             PoseStamped,
             '/vrpn_client_node/dashgo/pose',
-            self.listener_callback,
+            self.optitrack_callback,
             10)
         self.subscription  # prevent unused variable warning
 
-        self.visual_odom_pub = self.create_publisher(VehicleVisualOdometry, '/VehicleVisualOdometry_PubSubTopic', 10)
 
-    def listener_callback(self, msg):
+        self.timesync_sub = self.create_subscription(Timesync, 'Timesync_PubSubTopic', self.timesync_callback, 10)
+        #self.visual_odom_pub = self.create_publisher(VehicleVisualOdometry, '/VehicleVisualOdometry_PubSubTopic', 10)
+        self.mocap_odom_pub = self.create_publisher(VehicleMocapOdometry, '/VehicleMocapOdometry_PubSubTopic', 10)
+
+
+
+    def timesync_callback(self, msg):
         print(msg)
+
+    def optitrack_callback(self, msg):
+        #print ("Publishing odom messages")
 
         seconds = int (msg.header.stamp.sec)
         nanoseconds = int (msg.header.stamp.nanosec)
 
-        visual_odom_msg = VehicleVisualOdometry()
-        visual_odom_msg.x = msg.pose.position.x
-        visual_odom_msg.y = msg.pose.position.y
-        visual_odom_msg.z = msg.pose.position.z
-        visual_odom_msg.timestamp = seconds * 10**6 + int (nanoseconds / 1000)
-        visual_odom_msg.q[0] = msg.pose.orientation.x
-        visual_odom_msg.q[1] = msg.pose.orientation.y
-        visual_odom_msg.q[2] = msg.pose.orientation.z
-        visual_odom_msg.q[3] = msg.pose.orientation.w
+        # visual_odom_msg = VehicleVisualOdometry()
+        
+        # visual_odom_msg.x = msg.pose.position.x
+        # visual_odom_msg.y = msg.pose.position.y
+        # visual_odom_msg.z = msg.pose.position.z
+        # visual_odom_msg.timestamp = seconds * 10**6 + int (nanoseconds / 1000)
+        # visual_odom_msg.q[0] = msg.pose.orientation.x
+        # visual_odom_msg.q[1] = msg.pose.orientation.y
+        # visual_odom_msg.q[2] = msg.pose.orientation.z
+        # visual_odom_msg.q[3] = msg.pose.orientation.w
 
-        self.visual_odom_pub.publish(visual_odom_msg)
+        # self.visual_odom_pub.publish(visual_odom_msg)
 
+        mocap_odom_msg = VehicleMocapOdometry()
+        mocap_odom_msg.x = msg.pose.position.x
+        mocap_odom_msg.y = msg.pose.position.y
+        mocap_odom_msg.z = msg.pose.position.z
+        mocap_odom_msg.timestamp = seconds * 10**6 + int (nanoseconds / 1000)
+        mocap_odom_msg.q[0] = msg.pose.orientation.x
+        mocap_odom_msg.q[1] = msg.pose.orientation.y
+        mocap_odom_msg.q[2] = msg.pose.orientation.z
+        mocap_odom_msg.q[3] = msg.pose.orientation.w
 
-        self.visual_odom_pub.publish
+        self.mocap_odom_pub.publish(mocap_odom_msg)
+
+    
 
     
 
