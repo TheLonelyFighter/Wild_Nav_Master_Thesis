6cbb3d5d2729dc85ef45b61640c277c9571689d5
TheLonelyFighter <mihaiandrei1987@yahoo.com>
2022-05-31 17:17:00 +0300

    read metadata from different cameras

--- src/extract_image_meta_exif.py
+++ src/extract_image_meta_exif.py
@@ -3,9 +3,11 @@ import re
 import os
 
 
-csv_filename = "matrice_wide_session_1.txt"
+csv_filename = "racer_drone.csv"
 
-photo_folder = '/home/marius/Desktop/4_may_dataset/matrice_300/matrice_wide_session_1/'
+photo_folder = '/home/marius/Desktop/4_may_dataset/small_dji_mini_2/100MEDIA/'
+ 
+csv_filename = photo_folder + csv_filename
 
 def convert_gnss_coord(lat_or_lon):
     deg, deg_string, minutes,discard_1,  seconds, discard_2, direction =  re.split('[\s°\'"]', lat_or_lon)
@@ -14,17 +16,16 @@ def convert_gnss_coord(lat_or_lon):
 
 def load_images_from_folder(folder):
     images_list = []
+    print("Loading images")
     for filename in os.listdir(folder):
-        images_list.append(filename)
+        if filename.endswith((".JPG", ".jpg", ".png")):
+            images_list.append(filename)
     return images_list
     
 
-converted = convert_gnss_coord('''60 deg 24' 8.13" N''')
-print(converted)
-
 
 images_list = load_images_from_folder(photo_folder)
-print(images_list)
+#print(images_list)
 
 f = open(csv_filename, "a")
 
@@ -32,6 +33,7 @@ filesize = os.path.getsize(csv_filename)
 if filesize == 0:
     f.write("Filename,Latitude,Longitude,Altitude,Gimball_Roll,Gimball_Yaw,Gimball_Pitch,Flight_Roll,Flight_Yaw,Flight_Pitch")
 
+print("Reading metadata")
 for image in images_list:
     infoDict = {} #Creating the dict to get the metadata tags
     exifToolPath = 'exiftool'
@@ -41,17 +43,49 @@ for image in images_list:
     for tag in process.stdout:
         line = tag.strip().split(':')
         infoDict[line[0].strip()] = line[-1].strip()
+        print(line[0].strip())
 
-    filename= infoDict['File Name']
-    lat = infoDict['GPS Latitude']
-    lon = infoDict['GPS Longitude']
-    altitude = infoDict['Relative Altitude']
-    gimball_roll = infoDict['Gimbal Roll Degree']
-    gimball_yaw = infoDict['Gimbal Yaw Degree']
-    gimball_pitch = infoDict['Gimbal Roll Degree']
-    flight_roll = infoDict['Flight Roll Degree']
-    flight_yaw = infoDict['Flight Yaw Degree']
-    flight_pitch = infoDict['Flight Pitch Degree']
+    print ("################################################3")
+
+    #Default values
+    altitude = "NaN"
+    gimball_roll = "NaN"
+    gimball_yaw = "NaN"
+    gimball_pitch = "NaN"
+    flight_roll = "NaN"
+    flight_yaw = "NaN"
+    flight_pitch = "NaN"
+
+
+    #print(infoDict['GPS Altitude'])
+    print(infoDict['File Name'])
+    if 'File Name' in infoDict:
+        filename = infoDict['File Name']
+        print(filename)
+    if 'GPS Latitude' in infoDict:
+        lat = infoDict['GPS Latitude']
+    if 'GPS Longitude' in infoDict:
+        lon = infoDict['GPS Longitude']
+    if 'Relative Altitude' in infoDict:
+        altitude = infoDict['Relative Altitude']
+    elif 'GPS Altitude' in infoDict:
+        altitude = infoDict['GPS Altitude']
+        print(infoDict['GPS Altitude'])
+        altitude =  re.search(r'\d+', altitude).group() #keep only the number value, discard text
+        print(altitude)
+
+    if 'Gimbal Roll Degree' in infoDict:
+        gimball_roll = infoDict['Gimbal Roll Degree']
+    if 'Gimbal Yaw Degree' in infoDict:
+        gimball_yaw = infoDict['Gimbal Yaw Degree']
+    if 'Gimbal Pitch Degree' in infoDict:
+        gimball_pitch = infoDict['Gimbal Pitch Degree']
+    if 'Flight Roll Degree' in infoDict:
+        flight_roll = infoDict['Flight Roll Degree']
+    if 'Flight Yaw Degree' in infoDict:
+        flight_yaw = infoDict['Flight Yaw Degree']
+    if 'Flight Pitch Degree' in infoDict:    
+        flight_pitch = infoDict['Flight Pitch Degree']
 
     lat = convert_gnss_coord(lat)
     lon = convert_gnss_coord(lon)
